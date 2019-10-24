ddc8c4643144ea2d026c0710d7608502b2defe4e
DodgeV <49369085+DodgeV@users.noreply.github.com>
2019-10-24 21:29:57 +0800

    Update csv_writer.py

--- csv_writer.py
+++ csv_writer.py
@@ -3,7 +3,7 @@ def creat_csv_file(path):
     head = ['name','sex','age']
     body = [('lucy','girl','14'),
             ('jack','boy','17')]
-    with open(path,'w',newline='') as f:
+    with open(path,'w',newline='') as f: #指定newline=‘’，可以防止向文件中写入空行
         a = csv.writer(f)
         a.writerow(head)
         a.writerows(body)
