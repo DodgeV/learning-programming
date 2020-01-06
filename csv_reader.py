1b14398b2c56d23bd14d43a7c90430a06bb69bc6
DodgeV <49369085+DodgeV@users.noreply.github.com>
2020-01-06 17:53:44 +0800

    Delete csv_reader.py

--- csv_reader.py
+++
@@ -1,9 +0,0 @@
-import csv
-def get_content(path):
-    with open(path,'r',newline = '') as f:
-        headers = next(csv.reader(f))
-        print(headers)
-        for i in csv.reader(f):
-            print(i)
-if __name__ == '__main__':
-    get_content(r'csvfile.csv')
